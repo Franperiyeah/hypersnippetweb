1ea36880227f958008d11ec8d9ec2da6ff328512
Franperiyeah <56832698+Franperiyeah@users.noreply.github.com>
2021-10-30 18:06:11 +0200

    started hyper snippets web

---
+++ hypersnippetweb/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header class="header_header__HDboe container">
+<a class="header_logo__2lzAa" aria-label="Hyper logo" href="/"><img width="31" height="23" src={logo} alt="Hyper logo" /> hyper snippets</a>
+<nav class="header_desktopNav__1U-xV">
+<a target="_blank" rel="noopener noreferrer" href="https://marketplace.visualstudio.com/items?itemName=FranRuizSantaclara.hypersnippets" class="header_link__21Pi4">Marketplace</a>
+<a target="_blank" rel="noopener noreferrer" href="https://github.com/vercel/hyper" class="header_link__21Pi4">GitHub</a>
+     </nav>
+      </header>
+      <main>
+        <div class="jsx-2675718438 hero_root__1FlFk">
+        <img class="hero_logo__2EG65" width="63" height="60" src={logo} alt="Hyper logo" />
+        
+        </div>
+        <div class="jsx-2675718438 content_root__1KEov">
+        <h2 class="jsx-2675718438 installation_title__24KUc"> The power of snippets </h2>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
