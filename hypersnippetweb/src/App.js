390881d4a39127b8a96680544903ce9cdc39253d
Franperiyeah <56832698+Franperiyeah@users.noreply.github.com>
2021-10-31 19:07:52 +0100

    Added new content

--- hypersnippetweb/src/App.js
+++ hypersnippetweb/src/App.js
@@ -8,7 +8,7 @@ function App() {
 <a class="header_logo__2lzAa" aria-label="Hyper logo" href="/"><img width="31" height="23" src={logo} alt="Hyper logo" /> hyper snippets</a>
 <nav class="header_desktopNav__1U-xV">
 <a target="_blank" rel="noopener noreferrer" href="https://marketplace.visualstudio.com/items?itemName=FranRuizSantaclara.hypersnippets" class="header_link__21Pi4">Marketplace</a>
-<a target="_blank" rel="noopener noreferrer" href="https://github.com/vercel/hyper" class="header_link__21Pi4">GitHub</a>
+<a target="_blank" rel="noopener noreferrer" href="https://github.com/Franperiyeah/hypersnippetweb" class="header_link__21Pi4">GitHub</a>
      </nav>
       </header>
       <main>
@@ -18,7 +18,45 @@ function App() {
         </div>
         <div class="jsx-2675718438 content_root__1KEov">
         <h2 class="jsx-2675718438 installation_title__24KUc"> The power of snippets </h2>
+        <p class="jsx-2675718438">Hyper snippets is a Visual studio code extension that contains HTML, Javascript, React, Angular, Vue and CSS snippets.</p>
+   
         </div>
+        <div class="jsx-2675718438 content_root__1KEov">
+        <h2 class="jsx-2675718438 installation_title__24KUc"> Some examples </h2>
+        <p class="jsx-2675718438">Hyper snippets is a Visual studio code extension that contains HTML, Javascript, React, Angular, Vue and CSS snippets.</p>
+        <table>
+  <tr>
+    <th>Code React</th>
+    <th>Description</th>
+  </tr>
+  <tr>
+    <td>!hyprsfc</td>
+    <td>stateless function component</td>
+  </tr>
+  <tr>
+    <td>!hyprcdm</td>
+    <td>componentDidMount</td>
+  </tr>
+  <tr>
+    <td>!hyprc</td>
+    <td>React component template.</td>
+  </tr>
+  <tr>
+    <td>!hyprcc</td>
+    <td>Class Component and export.</td>
+  </tr>
+</table>
+        </div>
+        <div class="jsx-2675718438 content_root__1KEov margin-bottom">
+        <h2 class="jsx-2675718438 installation_title__24KUc"> Do you want to collaborate? </h2>
+        <p class="jsx-2675718438">If you want to collaborate on this project with snippets that you think may be useful to users around the world, you are more than welcome to get in touch through the Github repository!</p>
+         </div>
+        
+        
+        
+        
+        
+        
       </main>
     </div>
   );
